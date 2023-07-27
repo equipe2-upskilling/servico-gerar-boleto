512201eb7f8048339bfa631ae1810180afd0673d
Alberto Santamaria <alberto.santamaria@animaeducacao.com.br>
2023-07-27 15:09:03 -0300

    Adicionar arquivos de projeto.

---
+++ servico-gerar-boleto/ConsumerSQS/ConsumerAmazonWS.cs
@@ -0,0 +1,176 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using iText.Html2pdf;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Utils;
+using Org.BouncyCastle.Crypto.Tls;
+using System;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Student.ConsumerAWS.ConsumerSQS
+{
+    public class ConsumerAmazonWS
+    {
+        private readonly AmazonSQSClient _sqsClient;
+        private readonly string? _queueUrl;
+
+        public ConsumerAmazonWS(string awsAccessKeyId, string awsSecretAccessKey, string awsRegion, string queueName)
+        {
+            // Configuração do cliente SQS
+            var sqsConfig = new AmazonSQSConfig
+            {
+                RegionEndpoint = RegionEndpoint.GetBySystemName(awsRegion)
+            };
+
+            _sqsClient = new AmazonSQSClient(awsAccessKeyId, awsSecretAccessKey, sqsConfig);
+
+            // Obtém a URL da fila pelo nome
+            _queueUrl = GetQueueUrlByName(queueName).GetAwaiter().GetResult();
+        }
+
+        public async Task StartListening(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var request = new ReceiveMessageRequest
+                    {
+                        QueueUrl = _queueUrl,
+                        MaxNumberOfMessages = 10, // Número máximo de mensagens a serem recebidas de uma vez
+                        WaitTimeSeconds = 20 // Tempo de espera (em segundos) para receber novas mensagens
+                    };
+
+                    var response = await _sqsClient.ReceiveMessageAsync(request, cancellationToken);
+
+                    if (response.Messages.Count > 0)
+                    {
+                        foreach (var message in response.Messages)
+                        {
+                            // Processar a mensagem recebida aqui
+                            Console.WriteLine($"Mensagem Recebida: {message.Body}");
+
+                            await SendPdfRequest();
+                            // Deletar a mensagem da fila após processamento
+                            await DeleteMessageFromQueue(message.ReceiptHandle, cancellationToken);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Trate a exceção aqui ou apenas lance-a novamente para ser tratada externamente
+                    Console.WriteLine($"Erro ao receber mensagens da fila SQS: {ex.Message}");
+                }
+            }
+        }
+
+        private async Task SendPdfRequest()
+        {
+            // Open text file
+            FileStream htmlSource = File.Open("..\\..\\..\\templateBoleto.html", FileMode.Open);
+
+            // Create PDF file
+            FileStream pdfDest = File.Open("boleto.pdf", FileMode.OpenOrCreate);
+
+            // Intialize conversion properties
+            ConverterProperties converterProperties = new ConverterProperties();
+            HtmlConverter.ConvertToPdf(htmlSource, pdfDest, converterProperties);
+
+            Console.WriteLine("PDF do boleto gerado com sucesso!");
+
+            // Send the PDF file to the QueuePutBoleto queue
+            // Caminho do arquivo PDF que deseja enviar
+            var filePath = "/boleto.pdf";
+
+            // Verifique se o arquivo PDF existe
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Arquivo PDF do boleto não encontrado.");
+                return;
+            }
+            try
+            {
+                // Leitura do arquivo PDF para um array de bytes
+                byte[] fileBytes = File.ReadAllBytes(filePath);
+
+                // Envio do arquivo para a fila SQS
+                var sendMessageRequest = new SendMessageRequest
+                {
+                    QueueUrl = _queueUrl,
+                    MessageBody = Convert.ToBase64String(fileBytes) // Enviar o arquivo codificado em base64
+                };
+
+                var response = await _sqsClient.SendMessageAsync(sendMessageRequest);
+
+                Console.WriteLine($"Arquivo de boleto enviado com sucesso para a fila SQS. MessageId: {response.MessageId}");
+            }
+            catch (AmazonSQSException ex)
+            {
+                Console.WriteLine($"Erro ao enviar o arquivo de boleto para a fila SQS: {ex.Message}");
+            }
+
+        }
+
+        private async Task SendMessageToQueue(string messageBody)
+        {
+            try
+            {
+                var request = new SendMessageRequest
+                {
+                    QueueUrl = "QueuePutBoleto", //  URL para enviar Pdf
+                    MessageBody = messageBody,
+                };
+
+                var response = await _sqsClient.SendMessageAsync(request);
+                Console.WriteLine($"Mensagem enviado with MessageId: {response.MessageId}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao enviar mensagem da fila SQS: {ex.Message}");
+            }
+        }
+
+
+        private async Task DeleteMessageFromQueue(string receiptHandle, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var request = new DeleteMessageRequest
+                {
+                    QueueUrl = _queueUrl,
+                    ReceiptHandle = receiptHandle
+                };
+
+                await _sqsClient.DeleteMessageAsync(request, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao deletar mensagem da fila SQS: {ex.Message}");
+            }
+        }
+
+        private async Task<string> GetQueueUrlByName(string queueName)
+        {
+            try
+            {
+                var request = new GetQueueUrlRequest
+                {
+                    QueueName = queueName
+                };
+
+                var response = await _sqsClient.GetQueueUrlAsync(request);
+                return response.QueueUrl;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ servico-gerar-boleto/Program.cs
@@ -0,0 +1,30 @@
+﻿using iText.Html2pdf;
+
+using Student.ConsumerAWS.ConsumerSQS;
+using System.Configuration;
+
+// Configurações da AWS
+string? awsAccessKeyId = ConfigurationManager.AppSettings["AWSAccessKeyId"];
+string? awsSecretAccessKey = ConfigurationManager.AppSettings["AWSSecretAccessKey"];
+string? awsRegion = ConfigurationManager.AppSettings["AWSRegion"];
+string? queueName = ConfigurationManager.AppSettings["QueueName"];
+
+if (awsAccessKeyId == null) throw new Exception("Chave de Acesso faltando.");
+if (awsSecretAccessKey == null) throw new Exception("Segredo da Chave de Acesso faltando.");
+if (awsRegion == null) throw new Exception("Região faltando.");
+if (queueName == null) throw new Exception("Nome da fila faltando.");
+
+// Criação do ConsumerAWS
+var consumer = new ConsumerAmazonWS(awsAccessKeyId, awsSecretAccessKey, awsRegion, queueName);
+
+// Token de cancelamento para parar o consumo
+using var cancellationTokenSource = new CancellationTokenSource();
+
+// Iniciar o consumo de mensagens em um loop infinito com intervalo de 10 segundos
+while (!cancellationTokenSource.Token.IsCancellationRequested)
+{
+    Console.WriteLine("Esperando Mensagem para gerar boleto....");
+    await consumer.StartListening(cancellationTokenSource.Token);
+    await Task.Delay(TimeSpan.FromSeconds(10), cancellationTokenSource.Token);
+}
+
